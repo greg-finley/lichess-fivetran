725ad34b36158e04b321adbe9788b345ff828bf4
Greg Finley <gregory.finley@gmail.com>
2024-03-23 22:39:18 -0700

    sleep only on rate limits

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class HttpClient:
 http_client = HttpClient()
 
 
-def get_user_games(user_name: str, since: int):
+def get_user_games(user_name: str, since: int, is_retry: bool = False):
     url = f"https://lichess.org/api/games/user/{user_name}"
     response = http_client.get(
         url,
@@ -50,6 +50,11 @@ def get_user_games(user_name: str, since: int):
             "lastFen": "true",
         },
     )
+    if response.status_code == 429:
+        if is_retry:
+            response.raise_for_status()
+        sleep(60)
+        return get_user_games(user_name, since, is_retry=True)
     response.raise_for_status()
     return [json.loads(game) for game in response.text.strip().split("\n") if game]
 
@@ -74,11 +79,7 @@ def main(request):
     print(f"Old state: {old_state}")
     games = []
     has_more = False
-    first_user = True
     for user in USERS:
-        if not first_user:
-            sleep(60)
-        first_user = False
         last_epoch = old_state.get(user, OLDEST_EPOCH)
         user_games = get_user_games(user, last_epoch)
         if user_games:
